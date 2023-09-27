5bd824dc650c1311916b81d45e58951454714571
ADMIN <ADMIN@DESKTOP-17E74ON>
2023-09-27 19:49:30 +0530

    commit third

--- demogit/src/main/java/com/demo/demogit/App.java
+++ demogit/src/main/java/com/demo/demogit/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello  Navneet " );
+        System.out.println( "Hello  Navneet First commit");
     }
 }
